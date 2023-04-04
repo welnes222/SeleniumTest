d987ba773f3a9fada6a32378b4c20f82fc1461fa
welnes222 <rokas222@hotmail.com>
2023-04-05 02:00:06 +0300

    pataisa

--- src/test/java/EasySeleniumTest.java
+++ src/test/java/EasySeleniumTest.java
@@ -4,10 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.time.Duration;
+
+import static org.junit.Assert.*;
 
 public class EasySeleniumTest {
     public WebDriver driver = new ChromeDriver();
@@ -177,60 +176,104 @@ public class EasySeleniumTest {
         assertTrue(tick.contains("glyphicon-remove"));
     }
     @Test
-    public void EmptyFirstNameTest() {
-        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/div/input")).sendKeys("a  ");
-        String stl = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/small[1]")).getAttribute("style");
-        assertTrue(stl.contains("block"));
+    public void emptyFirstNameErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/small[2]")).getText(), "Please supply your first name");
     }
-//    @Test
-//    public void tickmarkLastNameTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/div/input")).sendKeys("Kontautaitis");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void emailTickmarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/div/input")).sendKeys("kontautas@pastas.com");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void phoneTickmarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/div/input")).sendKeys("869488741");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void addressTickmarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/div/input")).sendKeys("kontautu g.12");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void cityTickMarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/div/input")).sendKeys("Havana");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void stateTickMarkTest() {
-//        Select drpState = new Select(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[7]/div/div/select")));
-//        drpState.selectByVisibleText("Kansas");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[7]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void zipCodeTickMarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/div/input")).sendKeys("4897");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
-//    @Test
-//    public void projectDescriptionTickMarkTest() {
-//        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/div/textarea")).sendKeys("pamparampamapam");
-//        String tick = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/i")).getAttribute("class");
-//        assertTrue(tick.contains("glyphicon-ok"));
-//    }
+    @Test
+    public void emptyLastNameErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/small[2]")).getText(), "Please supply your last name");
+    }
+    @Test
+    public void emptyEmailErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/small[1]")).getText(), "Please supply your email address");
+    }
+    @Test
+    public void emptyPhoneErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/small[1]")).getText(), "Please supply your phone number");
+    }
+    @Test
+    public void emptyAddressErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/small[2]")).getText(), "Please supply your street address");
+    }
+    @Test
+    public void emptyCityErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/small[2]")).getText(), "Please supply your city");
+    }
+    @Test
+    public void emptyZipCodeErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/div/input")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/small[1]")).getText(), "Please supply your zip code");
+    }
+    @Test
+    public void emptyProjectDescriptionErrorTest() {
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/div/textarea")).sendKeys(" ");
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/small[2]")).getText(), "Please supply a description of your project");
+    }
+    @Test
+    public void tooShortErrorFirstName(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/div/input")).sendKeys("aaa");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/small[1]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void tooShortErrorLastName(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/div/input")).sendKeys("a");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/small[1]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void enterValidEmailError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/div/input")).sendKeys("awdawwa");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/small[2]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void enterValidPhoneError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/div/input")).sendKeys("18648");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/small[2]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void tooShortValidAddressError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/div/input")).sendKeys("aaaaa");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/small[1]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void enterValidCityError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/div/input")).sendKeys("aaa");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/small[1]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void enterValidZipCodeError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/div/input")).sendKeys("22");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/small[2]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void enterValidDescriptionError(){
+        driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/div/textarea")).sendKeys("22");
+        String style = driver.findElement(By.xpath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/small[1]")).getAttribute("data-bv-result");
+        assertTrue(style.contains("INVALID"));
+    }
+    @Test
+    public void tickMarksTest(){
+
+    }
+
+
+
+
+
+
+
     @BeforeClass
     public void beforeClass() {
        // System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver111.exe");
